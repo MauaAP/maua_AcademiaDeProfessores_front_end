8930399ef7c68bec1114bf9f54339b9c02e47a04
lucapinheiro <lucapgomes11@gmail.com>
2024-06-07 00:14:17 -0300

    fix: update API endpoint URL to use HTTPS

--- src/paginas/moderador/EventosMod.js
+++ src/paginas/moderador/EventosMod.js
@@ -12,7 +12,7 @@ export default function ListaEventosMod({ itensMenu }) {
   useEffect(() => {
     const fetchEventos = async () => {
       try {
-        const response = await axios.get("http://18.228.10.97:3000/api/events/", {
+        const response = await axios.get("https://gmerola.com.br/ap/api/events/", {
           headers: {
             Authorization: `Bearer ${localStorage.getItem("token")}`
           }
