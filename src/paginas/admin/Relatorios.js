7e357db610deb84d1cc1d5c371e23b0fdc5ea589
gabrielmerola <gabrielmerola12@gmail.com>
2024-09-15 08:46:57 -0300

    add bearer token to relatorios

--- src/paginas/admin/Relatorios.js
+++ src/paginas/admin/Relatorios.js
@@ -3,6 +3,7 @@ import NavBar from "../../componentes/navbar/navbar";
 import Relatorio from "../../componentes/corpo_relatorio/relatorio";
 import axios from "axios";  
 import { toast } from 'react-toastify';
+// import { CorpoDownloadRelatorio } from "../../componentes/corpo_download_relatorio";
 
 export default function Relatorios ({itensMenu}){
     const [certificadosData, setCertificadosData] = useState([]);
@@ -12,7 +13,11 @@ export default function Relatorios ({itensMenu}){
     useEffect(() => {
         const fetchCertificados = async () => {
         try {
-            const response = await axios.get("https://serene-mountain-65884-1b703ae41d98.herokuapp.com/api/presences");
+            const response = await axios.get("https://serene-mountain-65884-1b703ae41d98.herokuapp.com/api/presences", {
+                headers: {
+                   Authorization: `Bearer ${localStorage.getItem("token")}`
+                }
+            });
             // console.log(response.data);
             setCertificadosData(response.data);
         } catch (error) {
@@ -28,6 +33,7 @@ export default function Relatorios ({itensMenu}){
         <body>
             <NavBar itensMenu={itensMenu} cor={"#4F1313"}/>
             <Relatorio certificadosData={certificadosData} showProfessorSearch={true} />
+            {/* <CorpoDownloadRelatorio /> */}
         </body>
     );
 }
