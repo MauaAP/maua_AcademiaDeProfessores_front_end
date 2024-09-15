d709e68fa7a89e4ae5efee11dd514e287fb3a503
gabrielmerola <gabrielmerola12@gmail.com>
2024-09-15 08:46:07 -0300

    add bearer token

--- src/paginas/professores/Home_Professor.js
+++ src/paginas/professores/Home_Professor.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import NavBar from "../../componentes/navbar/navbar";
 import Certificados from "../../componentes/corpo_certificados/certificados";
 import axios from "axios";
-import {jwtDecode} from "jwt-decode";
 import { toast } from 'react-toastify';
 
 export default function HomeProfessor({ itensMenu }) {
@@ -10,13 +9,14 @@ export default function HomeProfessor({ itensMenu }) {
 
     const notierror = () => toast.error('Erro ao buscar os eventos!');
 
-    const decodedToken = jwtDecode(localStorage.getItem("token"));
-
     useEffect(() => {
         const fetchCertificados = async () => {
             try {
-                const response = await axios.get("https://serene-mountain-65884-1b703ae41d98.herokuapp.com/api/presences");
-
+                const response = await axios.get("https://serene-mountain-65884-1b703ae41d98.herokuapp.com/api/presences", {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem("token")}`
+                    }
+                });
                 setCertificadosData(response.data);
             } catch (error) {
                 console.error("Erro ao buscar eventos:", error);
@@ -27,12 +27,10 @@ export default function HomeProfessor({ itensMenu }) {
         fetchCertificados();
     }, []);
 
-    const filteredCertificados = certificadosData ? certificadosData.filter(certificado => certificado.userId === decodedToken.id) : [];
-
     return (
         <div>
             <NavBar itensMenu={itensMenu} cor={"#14134F"} />
-            <Certificados certificadosData={filteredCertificados} mostrarBusca={false} />
+            <Certificados certificadosData={certificadosData} mostrarBusca={false} />
         </div>
     );
 }
