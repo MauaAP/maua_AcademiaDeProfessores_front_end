d969afc45dce721f27205ed58a576bdf5b25d7ee
LucaPinheiro <lucapgomes11@gmail.com>
2024-10-21 23:02:52 -0300

    Refactor API endpoint URLs for event and user data retrieval

--- src/paginas/certificacoes/certificados.js
+++ src/paginas/certificacoes/certificados.js
@@ -11,7 +11,7 @@ export default function Certificacao() {
     const eventId = urlParams.get('eventId');
 
     useEffect(() => {
-        axios.get(`https://serene-mountain-65884-1b703ae41d98.herokuapp.com/api/events/${eventId}`)
+        axios.get(`https://6ymi3mpnkb.us-east-1.awsapprunner.com/api/events/${eventId}`)
         .then(response => {
             setEvento(response.data);
         })
@@ -24,7 +24,7 @@ export default function Certificacao() {
     useEffect(() => {
         const fetchUsers = async () => {
             try {
-                const response = await axios.get('https://serene-mountain-65884-1b703ae41d98.herokuapp.com/api/users-list');
+                const response = await axios.get('https://6ymi3mpnkb.us-east-1.awsapprunner.com/api/users-list');
                 setLista(response.data);
             } catch (error) {
                 console.error('Erro ao buscar usuários:', error);
