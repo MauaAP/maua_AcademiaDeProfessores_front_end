ecae05647a2e4b04520875f5a553392f811744bf
LucaPinheiro <lucapgomes11@gmail.com>
2024-10-21 23:02:39 -0300

    Refactor API endpoint for updating professor status URL

--- src/componentes/professor/professor.js
+++ src/componentes/professor/professor.js
@@ -37,7 +37,7 @@ export default function TemplateProfessor({ professor, cpf, phone, email, role, 
 
     const notiInativar = () => {
         toast.dismiss();
-        axios.put('https://serene-mountain-65884-1b703ae41d98.herokuapp.com/api/update-status', {
+        axios.put('https://6ymi3mpnkb.us-east-1.awsapprunner.com/api/update-status', {
             id: editedData.id,
             status: editedData.status === 'ACTIVE' ? 'INACTIVE' : 'ACTIVE'
         }, {
